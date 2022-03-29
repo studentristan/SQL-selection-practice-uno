21819b800de1843344239993848dbc15c379c1eb
Tristan Werden <54915507+studentristan@users.noreply.github.com>
2022-03-28 20:17:47 -0400

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -1,3 +1,7 @@
+-- Really hard inserty thngy (I kinda stole yours, but I understand it now lol)
+-- INSERT INTO Movie ( id, name, year, rating, runtime, genre, earnings_rank ) VALUES ( 4846340, 'Hidden Figures', 2016, 'PG', 127, 'BDH', NULL );
+
+
 -- Incredibles 2
 --SELECT earnings_rank, rating, runtime FROM Movie WHERE name = "Incredibles 2";
 
